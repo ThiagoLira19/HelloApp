f81aff6b7376ab035c8aaa5970b29d501bcca59b
Thiago Lira Da Silva <thiago.lira@vtal.com>
2024-12-05 20:22:56 -0300

    salvamento 05/12/2024 20:22

--- screens/Login.js
+++ screens/Login.js
@@ -1,21 +1,21 @@
 import React, { useState } from 'react';
-import { Button, TextInput, Text, View } from 'react-native';
-
-const executarLogin = () => {
-    alert('Quero Logar.')
-}
+import { Button, TextInput, Text, View, Alert } from 'react-native';
 
 const LoginScreen = () => {
 
     const [email, setEmail] = useState('');
     const [senha, setSenha] = useState('');
 
+    const executarLogin = () => {
+        alert(`Olá, ${email}`);
+    };
+
     return (
         <View>
             <Text>E-mail</Text>
-            <TextInput value={email} />
+            <TextInput value={email} onChangeText={setEmail}/>
             <Text>Senha</Text>
-            <TextInput value={senha} secureTextEntry />
+            <TextInput value={senha} onChangeText={setSenha} secureTextEntry/>
             <Button title='Entrar' onPress={executarLogin} />
         </View>
     );
